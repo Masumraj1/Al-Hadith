a8c144504f7d474cafb5622c743251f7082b1c07
masumraj <promotionbd321@gmail.com>
2025-06-26 02:58:56 +0600

    add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,21 @@ void main() {
 
 
 
+
+// import 'package:flutter/material.dart';
+// import 'package:device_preview/device_preview.dart';
+//
+// import 'app/core/utils/device_utils/device_utils.dart';
+// import 'my_app.dart';
+//
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   DeviceUtils.lockDevicePortrait();
+//
+//   runApp(
+//     DevicePreview(
+//       enabled: true, // release mode এ false করে দিও
+//       builder: (context) => const MyApp(), // <-- এখানে MyApp wrap করা হয়েছে
+//     ),
+//   );
+// }
